c9343b6592dd2f5e01f3e61c0063ed97e11932c6
cywisp <cywisp@gmail.com>
2024-04-24 19:32:12 -0400

    updated

--- upload_and_parse_s3/parser.py
+++ upload_and_parse_s3/parser.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import os
 import logging
+import pandas as pd
 from edi_835_parser import parse
+
 
 logging.basicConfig(
     format='%(asctime)s: %(message)s',
@@ -10,7 +12,6 @@ logging.basicConfig(
 log = logging.getLogger()
 
 FILE_PATH = 'upload'
-
 
 def get_files(file_path: str) -> list:
     try:
@@ -23,15 +24,26 @@ def get_files(file_path: str) -> list:
     except FileNotFoundError as e:
         log.error(f'No files found:\n{e}')
 
+
 def read_files(file_list: list) -> list:
+    log.info(f'Parsing all EDI files in {FILE_PATH}')
     content = list()
 
     for file in file_list:
         try:
-            transaction_set = parse(file)
+            content.append(parse(file).to_dataframe())
+        except Exception as e:
+            log.error(f'Unable to parse {file}:\n{e}')
 
+    return content
 
 
 if __name__ == '__main__':
-    log.info(get_files(FILE_PATH))
+    files = get_files(FILE_PATH)
+    log.info(files)
 
+    file_content = read_files(files)
+    log.info(f'Files read: {len(file_content)}')
+
+    log.info(file_content)
+    log.info('Done.')
